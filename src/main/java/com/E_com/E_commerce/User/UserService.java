aa939e58b9c2bff0a6be0ce50557006b2f82bbe5
lazyskiddie <sandeepsingh08550@gmail.com>
2026-09-11 16:09:10 +0530

    intermidate between controller and repository

--- src/main/java/com/E_com/E_commerce/User/UserService.java
+++ src/main/java/com/E_com/E_commerce/User/UserService.java
@@ -1,23 +1,23 @@
-package com.E_com.E_commerce;
+package com.E_com.E_commerce.User;
 
-import jakarta.persistence.OneToOne;
-import com.E_com.E_commerce.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class UserService {
 
+    // it will create the constructor using autowired
     @Autowired
     private UserRepository userRepository;
 
+    // it wiil find the user present in database and send it to the controllers
     public List<User> getalluser(){
         return userRepository.findAll();
     }
 
+    // it will take data from the controller and send to the repository
     public User addUser(User user) {
         return userRepository.save(user);
     }
