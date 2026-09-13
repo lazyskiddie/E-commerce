8ed71fd33b6afee36a0b1bee5919e412dfafad5c
lazyskiddie <sandeepsingh08550@gmail.com>
2026-09-13 20:46:04 +0530

    add the validation logic of notnull

--- src/main/java/com/E_com/E_commerce/User/User.java
+++ src/main/java/com/E_com/E_commerce/User/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "User Data for E-Comm")
@@ -15,7 +16,10 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull(message = "Please Enter the user name!!")
     private String username;
+    @NotNull(message = "Please Enter the password!!")
     private String password;
+    @NotNull(message = "Please Enter the email!!")
     private String email;
 }
