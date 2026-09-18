1cd51e956047fba559f2e74cc0ce95cd4c041da6
lazyskiddie <sandeepsingh08550@gmail.com>
2026-09-18 10:47:47 +0530

    onetoone ans joincolumn for thr address and the user entity

--- src/main/java/com/E_com/E_commerce/User/User.java
+++ src/main/java/com/E_com/E_commerce/User/User.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 import jakarta.validation.constraints.NotNull;
 
 @Entity
-@Table(name = "User Data for E-Comm")
+@Table(name = "User_Data_for_E-Comm")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -15,11 +15,22 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // it will generate the unique id foe each customer
     private Long id;
+    // uername for the customer
     @NotNull(message = "Please Enter the user name!!")
     private String username;
+
     @NotNull(message = "Please Enter the password!!")
     private String password;
     @NotNull(message = "Please Enter the email!!")
     private String email;
+    // it is the also a type of identifier for three class = "CUSTOMER", "MERCHANT", "ADMIN"
+    private UserRole role = UserRole.Customer;
+
+    // this make sure that if the user is deleted the address must also deleted
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "address_id", referencedColumnName = "id")
+    private Address address;
+
 }
