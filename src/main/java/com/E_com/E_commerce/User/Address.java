77fab973fc7fa741ad2e9ca508351be968ee78fc
lazyskiddie <sandeepsingh08550@gmail.com>
2026-09-18 10:46:35 +0530

    Address of the user

--- src/main/java/com/E_com/E_commerce/User/Address.java
+++ src/main/java/com/E_com/E_commerce/User/Address.java
@@ -1,4 +1,26 @@
 package com.E_com.E_commerce.User;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Address_for_user")
 public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String HouseNo;
+    private String street;
+    private String city;
+    private String state;
+    private String country;
+    private String zipCode;
 }
