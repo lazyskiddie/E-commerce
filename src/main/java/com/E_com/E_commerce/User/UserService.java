e6e5a4a4b60a1a4cceeacb84bb9204f8e25de7f0
lazyskiddie <sandeepsingh08550@gmail.com>
2026-09-18 12:01:29 +0530

    mapToUserResponse

--- src/main/java/com/E_com/E_commerce/User/UserService.java
+++ src/main/java/com/E_com/E_commerce/User/UserService.java
@@ -1,9 +1,13 @@
 package com.E_com.E_commerce.User;
 
+import com.E_com.E_commerce.User.dto.Addressdto;
+import com.E_com.E_commerce.User.dto.UserResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
@@ -13,8 +17,10 @@ public class UserService {
     private UserRepository userRepository;
 
     // it wiil find the user present in database and send it to the controllers
-    public List<User> getalluser(){
-        return userRepository.findAll();
+    public List<UserResponse> getalluser(){
+        return userRepository.findAll().stream()
+                .map(this::mapToUserResponse)
+                .collect(Collectors.toList());
     }
 
     // it will take data from the controller and send to the repository
@@ -23,9 +29,9 @@ public class UserService {
     }
 
     // this is for the user to fetch from the user "id"
-    public User finduserbyid(Long id) {
+    public Optional<UserResponse> finduserbyid(Long id) {
         return userRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("user not found!!"));
+                .map(this::mapToUserResponse);
     }
 
     // this will get the data for the user and find by user id and update the data of that user
@@ -37,6 +43,30 @@ public class UserService {
             userRepository.save(existingUser);
             return true;
         }).orElse(false);
+    }
 
+    private UserResponse mapToUserResponse(User user) {
+        if (user == null) {
+            return null;
+        }
+
+        UserResponse response = new UserResponse();
+        response.setId(String.valueOf(user.getId()));
+        response.setUsername(user.getUsername());
+        response.setEmail(user.getEmail());
+        response.setRole(user.getRole());
+
+        if (user.getAddress() != null) {
+            Addressdto addressDto = new Addressdto();
+            addressDto.setHouseNo(user.getAddress().getHouseNo());
+            addressDto.setStreet(user.getAddress().getStreet());
+            addressDto.setCity(user.getAddress().getCity());
+            addressDto.setState(user.getAddress().getState());
+            addressDto.setCountry(user.getAddress().getCountry());
+            addressDto.setZipCode(user.getAddress().getZipCode());
+            response.setAddress(addressDto);
+        }
+
+        return response;
     }
 }
