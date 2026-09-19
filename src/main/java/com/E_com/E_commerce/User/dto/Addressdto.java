eeb2789b595ef4b9cb85041837183894e30ac9c3
lazyskiddie <sandeepsingh08550@gmail.com>
2026-09-19 12:18:27 +0530

    dto of address

--- src/main/java/com/E_com/E_commerce/User/dto/Addressdto.java
+++ src/main/java/com/E_com/E_commerce/User/dto/Addressdto.java
@@ -1,4 +1,13 @@
 package com.E_com.E_commerce.User.dto;
 
+import lombok.Data;
+
+@Data
 public class Addressdto {
+    private String houseNo;
+    private String street;
+    private String city;
+    private String state;
+    private String country;
+    private String zipCode;
 }
