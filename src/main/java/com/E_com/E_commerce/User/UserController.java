b7f515457e578f08da60965f7f9207e173e7e1b0
lazyskiddie <sandeepsingh08550@gmail.com>
2026-09-19 12:20:55 +0530

    userRequest and updateUserFromRequest

--- src/main/java/com/E_com/E_commerce/User/UserController.java
+++ src/main/java/com/E_com/E_commerce/User/UserController.java
@@ -1,5 +1,6 @@
 package com.E_com.E_commerce.User;
 
+import com.E_com.E_commerce.User.dto.UserRequest;
 import com.E_com.E_commerce.User.dto.UserResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -21,9 +22,9 @@ public class UserController {
     }
 
     @PostMapping("api/user")
-    public ResponseEntity<User> createUser(@RequestBody User user){
-        userService.addUser(user);
-        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    public ResponseEntity<String> createUser(@RequestBody UserRequest userRequest){
+        userService.addUser(userRequest);
+        return ResponseEntity.ok().body("Success user added");
     }
 
     @GetMapping("/api/user/{id}")
@@ -32,11 +33,11 @@ public class UserController {
     }
 
     @PutMapping("api/user/{id}")
-    public ResponseEntity<User> updateUser(@PathVariable("id") Long id, @RequestBody User Updateduser){
-        boolean isUpdated = userService.updateuser(id, Updateduser);
+    public ResponseEntity<String> updateUser(@PathVariable("id") Long id, @RequestBody UserRequest updateUserRequest){
+        boolean isUpdated = userService.updateuser(id, updateUserRequest);
 
         if (isUpdated) {
-            return ResponseEntity.ok(Updateduser);
+            return ResponseEntity.ok("Updated user Successfully");
         } else {
             return ResponseEntity.notFound().build();
         }
--- src/main/java/com/E_com/E_commerce/User/UserService.java
+++ src/main/java/com/E_com/E_commerce/User/UserService.java
@@ -1,6 +1,7 @@
 package com.E_com.E_commerce.User;
 
 import com.E_com.E_commerce.User.dto.Addressdto;
+import com.E_com.E_commerce.User.dto.UserRequest;
 import com.E_com.E_commerce.User.dto.UserResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,7 +25,9 @@ public class UserService {
     }
 
     // it will take data from the controller and send to the repository
-    public User addUser(User user) {
+    public User addUser(UserRequest userRequest) {
+        User user = new User();
+        updateUserFromRequest(user, userRequest);
         return userRepository.save(user);
     }
 
@@ -35,14 +38,29 @@ public class UserService {
     }
 
     // this will get the data for the user and find by user id and update the data of that user
-    public boolean updateuser(Long id, User Updateduser) {
+    public boolean updateuser(Long id, UserRequest updateUserRequest) {
         return userRepository.findById(id).map(existingUser -> {
-            existingUser.setUsername(Updateduser.getUsername());
-            existingUser.setPassword(Updateduser.getPassword());
-            existingUser.setEmail(Updateduser.getEmail());
+            updateUserFromRequest(existingUser, updateUserRequest);
             userRepository.save(existingUser);
             return true;
         }).orElse(false);
+    }
+
+    private void updateUserFromRequest(User user, UserRequest userRequest) {
+        user.setUsername(userRequest.getUsername());
+        user.setPassword(userRequest.getPassword());
+        user.setEmail(userRequest.getEmail());
+
+        if(userRequest.getAddress() != null) {
+            Address address = new Address();
+            address.setHouseNo(userRequest.getAddress().getHouseNo());
+            address.setStreet(userRequest.getAddress().getStreet());
+            address.setCity(userRequest.getAddress().getCity());
+            address.setState(userRequest.getAddress().getState());
+            address.setCountry(userRequest.getAddress().getCountry());
+            address.setZipCode(userRequest.getAddress().getZipCode());
+            user.setAddress(address);
+        }
     }
 
     private UserResponse mapToUserResponse(User user) {
