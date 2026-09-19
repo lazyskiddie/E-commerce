16045f82e4057a651d0860dabe84ccb44db0eb13
lazyskiddie <sandeepsingh08550@gmail.com>
2026-09-19 12:19:13 +0530

    userRequest

--- src/main/java/com/E_com/E_commerce/User/dto/UserRequest.java
+++ src/main/java/com/E_com/E_commerce/User/dto/UserRequest.java
@@ -1,4 +1,11 @@
 package com.E_com.E_commerce.User.dto;
 
+import lombok.Data;
+
+@Data
 public class UserRequest {
+    private String username;
+    private String password;
+    private String email;
+    private Addressdto address;
 }
