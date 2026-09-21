f59a576e17f3262c1f0ce58bd1c3023700d5a8bc
lazyskiddie <sandeepsingh08550@gmail.com>
2026-09-21 09:17:00 +0530

    Repository to store the data and hardcoded the product availability

---
+++ src/main/java/com/E_com/E_commerce/Product/dto/ProductRequest.java
@@ -0,0 +1,4 @@
+package com.E_com.E_commerce.Product.dto;
+
+public class ProductRequest {
+}
--- src/main/java/com/E_com/E_commerce/Product/products.java
+++ src/main/java/com/E_com/E_commerce/Product/products.java
@@ -29,7 +29,7 @@ public class products {
     private String price;
     private String blockQuantity;
     private String imageurl;
-    private Boolean active;
+    private Boolean active = true;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
